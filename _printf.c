03e0a2dd7efbfb9c218cc5f70df47c549c8b7b33
HectorPR4546 <h_perez93@yahoo.com>
2025-03-28 15:07:18 -0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,75 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <stdarg.h>
-#include <unistd.h>
+#include <stddef.h>
+#include "main.h"
 
-typedef struct t
-{
-	char *s;
-	int (*f)(va_list argument);
-}type;
-
-int _putchar(int c)
-{
-	return write(1, &c, 1);
-}
-
-int print_d(long n, int base)
-{
-	int count;
-	char *symbols;
-
-	symbols = "0123456789abcdefABCDEF";
-	if (n < 0)
-	{
-		write(1, "-", 1);
-			return print_d(-n, base) + 1;
-	}
-	else if (n < base)
-	{
-		return _putchar(symbols[n]);
-	}
-	else
-	{
-		count = print_d(n / base, base);
-		return count + print_d(n % base, base);
-	}
-}
-
-
-int type_char_print(va_list argument)
-{
-	int ch = va_arg(argument, int);
-	return write(1, &ch, 1);
-}
-
-int type_string_print(va_list argument)
-{
-	char *arg = va_arg(argument, char*);
-	int j;
-
-	for (j = 0; arg[j] != '\0'; j++)
-		_putchar((int)arg[j]);
-	return(j);
-}
-
-int type_digit_print(va_list argument)
-{
-	int r = 0;
-
-	r += print_d((long)(va_arg(argument, int)), 10);
-	return (r);
-}
-
-int type_hexa_print(va_list argument)
-{
-	int r = 0;
-
-	r += print_d((long)(va_arg(argument, unsigned int)), 16);
-	return (r);
-}
-
-
+/**
+ * _print_spec - selects fuction based on specifier
+ * @spc: specifier to compare
+ * @argument: argument to use on selected fuction
+ * Return: result of selected fuction or 0 specifier doesnt match
+ */
 int _print_spec(char spc, va_list argument)
 {
 	int i;
@@ -93,6 +32,11 @@ int _print_spec(char spc, va_list argument)
 	return (0);
 }
 
+/**
+ * _printf - fuctions that outputs a string, temu printf
+ * @format: values to print
+ * Return: amount of chars printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list argument;
@@ -104,7 +48,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			ln = _print_spec(format[i + 1], argument);
-			if(ln != 0)
+			if (ln != 0)
 				i += 2;
 			else
 			{
